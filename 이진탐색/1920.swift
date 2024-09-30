623845649e11cff7ce36295ed58e032e7ec07ee1
sangwook16074 <qazws78941@gmail.com>
2024-09-30 19:21:06 +0900

    백준 1920 해결

--- 이진탐색/1920.swift
+++ 이진탐색/1920.swift
@@ -1,3 +1,22 @@
 func solution_1920() -> Void {
-    
+    let n = Int(readLine()!)!
+    let arr = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted(by: <)
+    let _ = Int(readLine()!)!
+    let targets = readLine()!.split(separator: " ").map { Int(String($0))! }
+    for t in targets {
+        var ans = 0
+        var (start, end) = (0, n-1)
+        while start <= end {
+            let mid = (start + end) / 2
+            if arr[mid] == t {
+                ans = 1
+                break
+            } else if arr[mid] < t {
+                start = mid + 1
+            } else if arr[mid] > t {
+                end = mid - 1
+            }
+        }
+        print(ans)
+    }
 }
