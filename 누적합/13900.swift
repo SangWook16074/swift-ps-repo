5d6072ddd98fe16f5820c21877d1fcafa651c525
sangwook16074 <qazws78941@gmail.com>
2024-10-31 20:20:35 +0900

    백준 21921 해결

---
+++ 누적합/13900.swift
@@ -0,0 +1,8 @@
+//
+//  13900.swift
+//  test
+//
+//  Created by 한상욱 on 10/31/24.
+//
+
+import Foundation
---
+++ 투 포인터/21921.swift
@@ -0,0 +1,8 @@
+//
+//  21921.swift
+//  test
+//
+//  Created by 한상욱 on 10/30/24.
+//
+
+import Foundation
