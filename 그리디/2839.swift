9f2ed915c98dcd11b7db9dfb637dbdebec348555
sangwook16074 <qazws78941@gmail.com>
2025-01-03 18:13:02 +0900

    백준 2839 해결

--- 그리디/2839.swift
+++ 그리디/2839.swift
@@ -1,8 +1,15 @@
-//
-//  2839.swift
-//  test
-//
-//  Created by 한상욱 on 1/1/25.
-//
+func solution_2839() {
+    let n = Int(readLine()!)!
+    var ans = 0
+    while n > 0 {
+        if n % 5 != 0 {
+            n -= 3
+            ans += 1
+        } else {
+            ans += n / 5
+            n = 0
+        }
+    }
+    print((ans = 0) ? ans : -1)
+}
 
-import Foundation
