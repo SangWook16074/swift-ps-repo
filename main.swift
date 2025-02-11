45baa3ceedff0de68da5f23a585b8287a8868f9b
sangwook16074 <qazws78941@gmail.com>
2025-02-11 18:45:02 +0900

    백준 1753 해결

--- main.swift
+++ main.swift
@@ -1,1 +1,177 @@
+import Foundation
 
+/**
+ made by 라이노
+ */
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+        // 인덱스 범위 넘어가는 것 방지
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+struct Heap<T: Comparable> {
+    private var elements: [T] = []
+    private let comparer: (T, T) -> Bool
+    
+    var isEmpty: Bool {
+        return elements.count <= 1
+    }
+    
+    var top: T? {
+        return isEmpty ? nil : elements[1]
+    }
+    
+    init(comparer: @escaping (T,T) -> Bool) {
+        self.comparer = comparer
+    }
+    
+    mutating func insert(_ element: T) {
+        if elements.isEmpty {
+            elements.append(element)
+            elements.append(element)
+            return
+        }
+        elements.append(element)
+        swimUp(index: elements.count - 1)
+    }
+    
+    mutating private func swimUp(index: Int) {
+        var index = index
+        while index > 1 && comparer(elements[index], elements[index / 2]) {
+            elements.swapAt(index, index / 2)
+            index /= 2
+        }
+    }
+    
+    mutating func pop() -> T? {
+        if elements.count < 2 { return nil }
+        elements.swapAt(1, elements.count - 1)
+        let deletedElement = elements.removeLast()
+        diveDown(index: 1)
+        return deletedElement
+    }
+    
+    mutating private func diveDown(index: Int) {
+        var swapIndex = index
+        var isSwap = false
+        let leftIndex = index * 2
+        let rightIndex = index * 2 + 1
+
+        if leftIndex < elements.endIndex && comparer(elements[leftIndex], elements[swapIndex]) {
+            swapIndex = leftIndex
+            isSwap = true
+        }
+        
+        if rightIndex < elements.endIndex && comparer(elements[rightIndex], elements[swapIndex]) {
+            swapIndex = rightIndex
+            isSwap = true
+        }
+
+        if isSwap {
+            elements.swapAt(swapIndex, index)
+            diveDown(index: swapIndex)
+        }
+    }
+}
+
+extension Heap {
+    init() {
+        self.comparer = (<)
+    }
+}
+
+struct Edge : Comparable {
+    let v : Int
+    let w : Int
+    
+    static func < (lhs : Edge, rhs : Edge) -> Bool {
+        return lhs.w < rhs.w
+    }
+}
+
+let fio = FileIO()
+let v = fio.readInt(), e = fio.readInt()
+let k = fio.readInt()
+var graph = [[(Int, Int)]](repeating: [(Int, Int)](), count: v + 1)
+let INF = Int.max
+var dist = [Int](repeating: INF, count: v + 1)
+for _ in 0 ..< e {
+    let u = fio.readInt(), v = fio.readInt(), w = fio.readInt()
+    graph[u].append((v, w))
+}
+
+func dijkstra(at start : Int) {
+    dist[start] = 0
+    var heapq = Heap<Edge>()
+    heapq.insert(Edge(v: start, w: 0))
+    while !heapq.isEmpty {
+        guard let now = heapq.pop() else { break }
+        if dist[now.v] < now.w { continue }
+        
+        for (v, w) in graph[now.v] {
+            let cost = w + now.w
+            if cost < dist[v] {
+                dist[v] = cost
+                heapq.insert(Edge(v: v, w: cost))
+            }
+        }
+    }
+}
+
+dijkstra(at: k)
+for i in 1 ... v {
+    print(dist[i] == INF ? "INF" : dist[i])
+}
--- src/Main.kt
+++ src/Main.kt
@@ -3,37 +3,44 @@ import kotlin.math.min
 
 fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
     fun nextInt() : Int { nextToken(); return nval.toInt() }
-    val n = nextInt(); val m = nextInt();
-    val INF = Long.MAX_VALUE
-    val dist = Array(n + 1) { LongArray(n + 1) { INF } }
+    val n = nextInt(); val m = nextInt()
+    val INF = 100
+    val graph = Array(n + 1) { mutableListOf<Int>() }
+    val dist = Array(n + 1) { IntArray(n + 1) {INF} }
     repeat(m) {
-        val a = nextInt(); val b = nextInt(); val c = nextInt()
-        dist[a][b] = min(dist[a][b], c.toLong())
-    }
-
-    repeat(n) {i ->
-        val idx = i + 1
-        dist[idx][idx] = 0
-    }
-
-    repeat(n) { k ->
-        repeat(n) { i ->
-            repeat(n) { j->
-                if (dist[i][k] != INF && dist[k][j] != INF && dist[i][k] + dist[k][j] < dist[i][k]) {
-                    dist[i][j] = dist[i][k] + dist[k][j]
-                }
-            }
+        val a = nextInt(); val b = nextInt()
+        if (!graph[a].contains(b)) {
+            graph[a].add(b)
         }
+        if (!graph[b].contains(a)) {
+            graph[b].add(a)
+        }
+    }
+    repeat(n) {i ->
+        dist[i + 1][i + 1] = 0
     }
 
     for (i in 1 .. n) {
-        for (j in 1 .. n) {
-            if (dist[i][j] == INF) {
-                print("0 ")
-            } else {
-                print("${dist[i][j]} ")
+        for (j in graph[i]) {
+            dist[i][j] = 1
+        }
+    }
+
+    for (k in 1 .. n) {
+        for (i in 1 .. n) {
+            for (j in 1 ..  n) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
             }
         }
-        println()
     }
+    val result = (1 .. n).map { i -> dist[i].reduce { acc, j -> acc + j } }
+    val minV = result.min()
+    var ans = 0
+    for (i in 0 ..< n) {
+        if (result[i] == minV) {
+            ans = i + 1
+            break
+        }
+    }
+    print(ans)
 }
