8635ea399780b9a598a8bae1fe502a09f95d26d8
sangwook16074 <qazws78941@gmail.com>
2025-10-11 13:22:18 +0900

    백준 11054 해결

---
+++ DP/11054.swift
@@ -0,0 +1,22 @@
+func solution_11054() {
+    let n = Int(readLine()!)!
+    let arr = readLine()!.split(separator: " ").compactMap { Int(String($0)) }
+    var dp = [[Int]](repeating: [1, 1], count: n)
+    for i in 0 ..< n {
+        for j in 0 ..< i {
+            if arr[i] > arr[j] { dp[i][0] = max(dp[i][0], dp[j][0] + 1) }
+        }
+    }
+
+    for i in stride(from: n - 1, to: -1, by: -1) {
+        for j in stride(from: n - 1, to: i, by: -1) {
+            if arr[i] > arr[j] { dp[i][1] = max(dp[i][1], dp[j][1] + 1) }
+        }
+    }
+
+    var ans = 0
+    for i in 0 ..< n {
+        ans = max(ans, dp[i][0] + dp[i][1] - 1)
+    }
+    print(ans)
+}
