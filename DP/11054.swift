469039fc1fe7552a842f2ce462a4618876bd39b2
sangwook16074 <qazws78941@gmail.com>
2025-10-12 23:19:00 +0900

    백준 11054 불필요한 연산 제거 리팩토링

--- DP/11054.swift
+++ DP/11054.swift
@@ -2,14 +2,14 @@ func solution_11054() {
     let n = Int(readLine()!)!
     let arr = readLine()!.split(separator: " ").compactMap { Int(String($0)) }
     var dp = [[Int]](repeating: [1, 1], count: n)
-    for i in 0 ..< n {
+    for i in 1 ..< n {
         for j in 0 ..< i {
             if arr[i] > arr[j] { dp[i][0] = max(dp[i][0], dp[j][0] + 1) }
         }
     }
 
     for i in stride(from: n - 1, to: -1, by: -1) {
-        for j in stride(from: n - 1, to: i, by: -1) {
+        for j in stride(from: n - 1, to: i - 1, by: -1) {
             if arr[i] > arr[j] { dp[i][1] = max(dp[i][1], dp[j][1] + 1) }
         }
     }
@@ -19,4 +19,5 @@ func solution_11054() {
         ans = max(ans, dp[i][0] + dp[i][1] - 1)
     }
     print(ans)
+
 }
